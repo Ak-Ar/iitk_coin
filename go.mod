fc884661fea3d6b2e3310922e046565a6c63047c
Akshat Arya <akshatarya20@iitk.ac.in>
2021-06-25 04:07:12 +0530

    created a database with sqlite3 to store data and added 'insertdata' function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Ak-Ar/iitk_coin
+
+go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.7
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+	createtable(db)
+	insertdata(db, "200083", "Akshat Arya")
+}
+
+func createtable(db *sql.DB) {
+	creatingtable := `CREATE TABLE IF NOT EXISTS student(
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"rollno" TEXT ,
+		"name" TEXT ,
+		unique(rollno));` //sql statement for creating table
+	statement, err := db.Prepare(creatingtable)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() //executing sql statements
+}
+
+func insertdata(db *sql.DB, rollno string, name string) {
+	insertingdata := `INSERT INTO student(rollno,name) VALUES (?,?)`
+	statement, err := db.Prepare(insertingdata)
+	statement.Exec(rollno, name)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
